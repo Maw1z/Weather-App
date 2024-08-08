3ae317dfb08f9b9be161cc9a431399bf166234cb
Mawiz <mohdmawiz11@gmail.com>
2024-08-08 12:51:05 +0300

    Created index.html and weather.html

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+async function fetchDailyWeather() {
+  const apiKey = "505b9f7c972bfae070f0eefbc9241b02";
+
+  try{
+    const city = document.getElementById('')
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**/*.{html,js}"],
+  content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+        secondary: "#000000",
+        accent1: {
+          900: "#545CFF",
+          100: "#C9CBFF",
+        },
+        accent2: "#DBDBDB",
+        grey1: "#D9D9D9",
+        grey2: "#434343",
+      },
+      screens: {
+        sm: "360px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+    },
   },
   plugins: [],
 };
